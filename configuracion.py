7ac4499c1009ef16996af52ef29e465462f5588b
Gerardo <gerardoguerralombardi@gmail.com>
2023-09-19 08:14:05 -0300

    Cambios en la velocidad de nave y balas

--- configuracion.py
+++ configuracion.py
@@ -6,9 +6,9 @@ class Configuracion:
         self.pantalla_ancho=1200
         self.pantalla_largo=800
         self.bg_color=(230, 230, 230) 
-        self.velocidad_nave = 10
+        self.velocidad_nave = 20
         #Configuracion de las balas
-        self.velocidad_bala=10
+        self.velocidad_bala=30
         self.ancho_bala=3
         self.largo_bala=15
         self.color_bala=(255,0,0)
